18eb931690da1c263dd98552d6a5aabd7fc80787
moaiii <c.melville@hotmail.co.uk>
2017-10-16 11:32:10 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Example
+
+```
+$ npm install crossfading-media-player
+```
+
+```
+import {MediaPlayer} from 'crossfading-media-player'
+```
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import crossfadingMediaPlayer from '.'
+
+test('says hello world', () => ( 
+  expect(crossfadingMediaPlayer()).toBe('Hello universe')
+))
